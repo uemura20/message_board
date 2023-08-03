78e0c791f855c481501196c5bf2d5691eabccfec
Yeonghyeon.Cho <choy@md-s.co.jp>
2023-08-03 15:36:51 +0900

    EncodingFilter修正

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -11,7 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
 /**
- * Servlet Filter implementation class EncodingFrilter
+ * Servlet Filter implementation class EncodingFilter
  */
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
@@ -20,24 +20,23 @@ public class EncodingFilter implements Filter {
      * Default constructor.
      */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
+
     }
 
     /**
      * @see Filter#destroy()
      */
     public void destroy() {
-        // TODO Auto-generated method stub
+
     }
 
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
@@ -45,7 +44,7 @@ public class EncodingFilter implements Filter {
      * @see Filter#init(FilterConfig)
      */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
+
     }
 
 }
